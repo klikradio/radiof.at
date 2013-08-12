da327799756c89eb3a0432a18be73bbfc1c47907
Jake Wood <jake@jakewood.org>
2013-08-11 22:41:29 -0700

    We'll inch our way there

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send("Welcome to radiof.at!");
+	res.send("Welcome to radiof.at! <form action='/' method='post'><input type='file' /><input type='submit' /></form>");
+});
+
+app.post('/', function(req, res) {
+	res.send(req.files);
 });
 
 app.listen(process.env.PORT || DEBUG_PORT);
