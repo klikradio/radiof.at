eac164e476b54fe3e6e6e9310f62a6d6a0223a7a
Jake Wood <jake@jakewood.org>
2013-08-11 22:48:46 -0700

    inch

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var DEBUG_PORT = 8000;
 
 var express = require('express');
+var sys = require('sys');
+var exec = require('child_process').exec;
 var app = express();
 
 app.use(express.bodyParser());
@@ -11,8 +13,14 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
 	res.send(req.files);
-	console.log('file uploaded');
-	console.log(req.files);
+
+	var tempFileName = req.files.song.path;
+	var realFileName = req.files.song.name;
+
+	exec('avconv -i "' + tempFileName + '" "' + tempFileName + '.wav"', function(err, stdout, stderr) {
+		console.log("We're done!!!");
+		console.log(tempFileName);
+	});
 });
 
 app.listen(process.env.PORT || DEBUG_PORT);
