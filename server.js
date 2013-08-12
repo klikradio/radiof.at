009ab92d7d84a5bb2b1776d5ba3b82fb23496570
jake wood <jake@jakewood.org>
2013-08-11 22:45:52 -0700

    inching along

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var DEBUG_PORT = 8000;
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
+
 app.get('/', function(req, res) {
-	res.send("Welcome to radiof.at! <form action='/' method='post'><input type='file' /><input type='submit' /></form>");
+	res.send("Welcome to radiof.at! <form action='/' method='post' enctype='multipart/form-data'><input type='file' name='song' /><input type='submit' /></form>");
 });
 
 app.post('/', function(req, res) {
 	res.send(req.files);
+	console.log('file uploaded');
+	console.log(req.files);
 });
 
 app.listen(process.env.PORT || DEBUG_PORT);
