3eaeb20b53edc36c8b580e00a73afb8e6e10afb7
jake wood <jake@jakewood.org>
2013-08-11 22:32:50 -0700

    add some docs to tags.py

--- tags.py
+++ tags.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 
+"""
+tags.py
+
+Converts the tags from an M4A file to an MP3 file
+
+usage:  ./tags.py <SOURCE FILE>.m4a <DESTINATION>.mp3
+
+This script only works in the M4A->MP3 direction.
+Other magic works for MP3->MP3.
+"""
+
 import sys
 from mutagen.easymp4 import EasyMP4
 from mutagen.easyid3 import EasyID3
